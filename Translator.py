43dac093ea0a9baa7131dd05ff015ffdbccaee77
Eric Lin <mummy4@sina.com>
2023-04-23 21:43:58 +0800

    Update Translator.py

--- Translator.py
+++ Translator.py
@@ -20,11 +20,6 @@ content = f.read()
 str_list = StringUtil.splitFromRows(content, rows)
 f.close()
 
-# prompt_str = f"##### Translate this code into {target_lang}\n### Code\n"+ content + f"\n### {target_lang}"
-# response = GPTUtil.createTextCompletion(api_key, model_id, prompt_str, 2048, 0, ["###"])
-# print(response)
-# output_str = json.loads(response)['choices'][0]['text']
-
 translated_code_chunks = []
 
 for i in range(len(str_list)):
